187992f6bea16aafabeb647e38f9acdd89ce2fce
alexchristison <alexchristison@gmail.com>
2023-04-25 17:12:14 -0400

    using hard coded word and letter to see if letter selected is true/false

--- js/main.js
+++ js/main.js
@@ -1,21 +1,97 @@
 
-// the function here is to randomly choose a word from the array under this function
-function dunk_6letter_words(items)
-{
-return items[Math.floor(Math.random()*items.length)]; 
-}
+/*------------Constants----------*/
+
+
+
 // random array of six letter words
-const items = ["orange", "apples", "poster", "burger", "pizzas", "litmus", "locker", "soccer", "bishop", "casual", "castle", "snazzy", "ornery", "walrus", "poncho", "velvet"];
+// const items = ["orange", "apples", "poster", "burger", "pizzas", "litmus", "locker", "soccer", "bishop", "casual", "castle", "snazzy", "ornery", "walrus", "poncho", "velvet"];
 
 // const correctLetter = [];
 // const wrongLetter = [];
 // const underScore = [];
 
-console.log(dunk_6letter_words(items));
+// console.log(dunk_6letter_words(items));
 
-function letterExistsInWord(word, letter) {
-    return word.indexOf(letter) !== -1;
-  }
+/*--------App's state (variables)----------*/
+// let randomWord = [items];
+// console.log(randomWord)
+// let correctLetter = '';
+// let wrongLetter = '';
+// let inputSelection = '';
+
+/*------------Cashed elements------------*/
+const letterButtonsEl = document.querySelector('#letters');
+
+// const docUnderScore = document.getElementsByClassName(underScore);
+// const docCorrectLetter = document.getElementsByClassName(correctLetter);
+// const docWrongLettter = document.getElementsByClassName(wrongLetter);
+/*-------------Event Listeners------------*/
+
+// this works for testing button!!! 
+letterButtonsEl.addEventListener('click', function(event) {
+    const buttonValue = event.target.textContent;
+  
+    console.log('Button clicked:', buttonValue);
+});
+
+
+// const docUnderScore = document.getElementsByClassName(underScore);
+// const docCorrectLetter = document.getElementsByClassName(correctLetter);
+// const docWrongLettter = document.getElementsByClassName(wrongLetter);
+
+/*-----------Functions-----------------*/
+// the function here is to randomly choose a word from the dunk 6 letter word array
+// function dunk_6letter_words(items)
+// {
+// let currentWord = items[Math.floor(Math.random()*items.length)]; 
+// console.log(currentWord)
+// return currentWord
+
+// }
+// console.log(currentWord)
+//////
+// Get user guess
+// const array = [items];
+
+// HARD CODED TEST FOR WORD / LETTER CHECK  up to 7 down.------>
+let currentWord = 'chopped'
+console.log(currentWord.includes.buttonValue);
+if (currentWord.includes('d') === true) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+// function to see if the letter the user inputs is in the word
+
+
+// this function may work once letter input is working? 
+// function letterInWord(word, letter) {
+//     return word.indexOf(letter) !== -1;
+//   }
+
+//   const chosenWord = dunk_6letter_words(items);
+
+  // create underscore array to show number of letters in a word
+// for (let i = 0; 1 < chosenWord.length; i++) {
+//     underScore.push("_");  
+// }
+// check if letter in a word
+// function checkLetter(letter) {
+//     if (letterExistInWord(chosenWord, letter)) {
+//         correctLetter.push(letter);
+//         // update underscore array to show a correctly guessed letter
+//     }
+//     for (let i = 0; i < chosenWord.length; i++) {
+//         if (chosenWord[i] === letter) {
+//         }
+//     }
+// }
+
+// console.log(chosenWord);
+// console.log(underScore); 
+
+
   // listen for event
   // if letter clicked is equal to a letter in the word (e.target) then the 
 
@@ -23,11 +99,9 @@ function letterExistsInWord(word, letter) {
 
 
 /*---------cashed elements-------------*/
-const letterButtonsEl = document.querySelector('#letters');
 
-// const docUnderScore = document.getElementsByClassName(underScore);
-// const docCorrectLetter = document.getElementsByClassName(correctLetter);
-// const docWrongLettter = document.getElementsByClassName(wrongLetter);
+
+
 
 // create underscore based on word length
 // const generateUnderScore = () => {
@@ -36,7 +110,6 @@ const letterButtonsEl = document.querySelector('#letters');
 //     }
 //     return underScore;
 // }
-
 /*----- event listeners -----*/
 // we're going to have a handle click function that manages all user interactions
 // with the alphabet buttons in the main section of HTML 
@@ -60,19 +133,5 @@ const letterButtonsEl = document.querySelector('#letters');
 // letterButtonsEl.addEventListener("click", function() {
 //     console.log("letterButtonsEl")
 // })
-
-letterButtonsEl.addEventListener('click', function(event) {
-    const buttonValue = event.target.textContent;
-  
-    console.log('Button clicked:', buttonValue);
-  });
-
-//  letterExistsInWord)
-
-
-
-
-// const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-//         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-//         't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
+// will use eventually once letter exists in word function up above 
+// letterButtonsEl.addEventListener('click', letterInWord)
